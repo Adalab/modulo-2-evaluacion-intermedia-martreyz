f02e75254f51093598744719ae17dca2926e260c
martreyz <martreyz@gmail.com>
2020-10-17 11:54:03 +0200

    Fix folders and files structure: add folder js to contain main.js

---
+++ js/main.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const numberInput = document.querySelector(".js-input");
+const inputButton = document.querySelector(".js-button");
+const hintText = document.querySelector(".js-hint");
+const triesText = document.querySelector(".js-tries");
+
+// Generates random number and show it in console:
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+let randomNumber = getRandomNumber(100);
+console.log("Mi número aleatorio es " + randomNumber);
+
+/*Listens button and reads input number, 
+compare with random generated and add tries to the counter:*/
+
+let tries = 0;
+
+function runGame(event) {
+  tries += 1;
+  triesText.innerHTML = "Número de intentos: " + tries;
+  const numberInputValue = parseInt(numberInput.value);
+  console.log("El número introducido es: " + numberInputValue);
+
+  if (numberInputValue === randomNumber) {
+    hintText.innerHTML = "Has ganado campeona!!!";
+  } else if (
+    numberInputValue < randomNumber &&
+    numberInputValue > 0 &&
+    numberInputValue <= 100
+  ) {
+    hintText.innerHTML = "Pista: Demasiado bajo";
+  } else if (
+    numberInputValue > randomNumber &&
+    numberInputValue > 0 &&
+    numberInputValue <= 100
+  ) {
+    hintText.innerHTML = "Pista: Demasiado alto";
+  } else if (numberInputValue < 1 || numberInputValue > 100) {
+    hintText.innerHTML = "Pista: El número debe estar entre 1 y 100";
+  }
+}
+
+inputButton.addEventListener("click", runGame);
+
+// Stops enter from refreshing the page and triggers button's click:
+
+function changeEnterAction(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    inputButton.click();
+  }
+}
+
+numberInput.addEventListener("keydown", changeEnterAction);
